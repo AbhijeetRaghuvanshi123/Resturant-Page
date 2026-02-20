42642d83d01c4bf82d20efec920cf616776cc144
abhijeet-raghuvanshi <abhijeetraghuvanshi9842abhi@gmail.com>
2026-02-20 13:55:14 +0530

    wrapped up with final contact tab. may revist to style

---
+++ src/contact.js
@@ -0,0 +1,31 @@
+const LoadContactPage = () => {
+    const content = document.querySelector("#content");
+    content.innerHTML = '';
+
+    const contact = document.createElement("div");
+    contact.classList.add("contact-info");
+
+    const h2 = document.createElement("h2");
+    h2.innerText = "Contact & Location";
+    contact.appendChild(h2);
+
+    const p1 = document.createElement("p");
+    p1.innerHTML = "<strong>Our Restaurant</strong>";
+    contact.appendChild(p1);
+
+    const p2 = document.createElement("p");
+    p2.innerText = "123 Flavor Street, Food City, Yummyland 45678";
+    contact.appendChild(p2);
+
+    const p3 = document.createElement("p");
+    p3.innerText = "Phone: (123) 456-7890";
+    contact.appendChild(p3);
+
+    const p4 = document.createElement("p");
+    p4.innerText = "Email: contact@ourrestaurant.com";
+    contact.appendChild(p4);
+
+    content.appendChild(contact);
+}
+
+export default LoadContactPage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import homepage from "./home.js"
 import menupage from "./menu.js"
+import contactpage from "./contact.js"
 
 const App = () => {
     homepage();
 
     document.getElementById("home-btn").addEventListener('click', () => homepage());
     document.getElementById("menu-btn").addEventListener('click', () => menupage());
+    document.getElementById("contact-btn").addEventListener('click', () => contactpage());
 }
 
 App();
